6975e0038163b9025c435f95d57a598fe9863ca6
Abdullah9511 <abdullah.bin.idrees@gmail.com>
2024-01-08 07:13:30 +0500

    Connecting APIs,Adding all funcionalities of notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,19 +1,141 @@
-import React,{useContext} from 'react'
+import React, { useContext, useEffect, useRef, useState } from "react";
+import Noteitem from "./Noteitem";
+import AddNote from "./AddNote";
 import NoteContext from "../context/notes/NoteContext";
-import Noteitem from './Noteitem';
 
 const Notes = () => {
-    const context = useContext(NoteContext);
-    const{notes,setNotes} = context;
-  return (
-    
-    <div className='container'>
-         <h1>Your Notes</h1>
-        {notes.map((note)=>{
-            return <Noteitem note = {note}/>
-        })}
-    </div>
-  )
-}
+  const context = useContext(NoteContext);
+  const { notes, getAllNotes, editNote} = context;
+  useEffect(() => {
+    getAllNotes();
+    // eslint-disable-next-line
+  }, []);
 
-export default Notes
+  const ref = useRef(null);
+  const refClose = useRef(null);
+  const [note, setNote] = useState({editTitle : "", editDescription : "", editTag : ""})
+  
+  const updateNote = (currentNote) => {
+    ref.current.click();
+    setNote({id : currentNote._id, editTitle :currentNote.title, editDescription : currentNote.description, editTag : currentNote.tag})
+
+  };
+
+  
+  const clickHandler = (e) => {
+    editNote(note.id,note.editTitle,note.editDescription,note.editTag);
+    refClose.current.click();
+    
+  } 
+  const onChange = (e) => {
+    setNote({...note,[e.target.name]:e.target.value})
+  } 
+
+  return (
+    <>
+      <AddNote/>
+
+      <button
+        ref={ref}
+        type="button"
+        className="btn btn-primary d-none"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Edit Note
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="editTitle"
+                    name="editTitle"
+                    value={note.editTitle}
+                    aria-describedby="emailHelp"
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="editDescription"
+                    name="editDescription"
+                    value={note.editDescription}
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="tag" className="form-label">
+                    Tag
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="editTag"
+                    name="editTag"
+                    value={note.editTag}
+                    onChange={onChange}
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+                ref={refClose}
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary" onClick={clickHandler}>
+                Update Note
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row my-3">
+        <h2>Your Notes</h2>
+        {notes.map((note) => {
+          return (
+            <Noteitem key={note._id} note={note} updateNote={updateNote} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Notes;
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -1,29 +1,104 @@
-import {  useState } from "react"
-import NoteContext from "./NoteContext"
+import { useState } from "react";
+import NoteContext from "./NoteContext";
 
+const NoteState = (props) => {
+  const host = "http://localhost:5000";
 
-const NoteState = (props) =>{
-    
-    const notesInitial = [
-        {
-          "_id": "65987a0ffcdd394ce25c3716",
-          "user": "65974b170898b413a493ebbb",
-          "title": "My title",
-          "description": "khana kha lia ha",
-          "tag": "Personal",
-          "timestamp": "2024-01-05T21:52:15.513Z",
-          "__v": 0
-        }
-      ]
-      
-      const [notes, setNotes] = useState(notesInitial)
-    return(
-        <NoteContext.Provider value={{notes, setNotes}}>
-            {props.children}
-        </NoteContext.Provider>
-    )
+  const notesInitial = [];
 
+  const [notes, setNotes] = useState(notesInitial);
 
-}
+  // Get all Notes
+  const getAllNotes = async () => {
+    const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+      method: "GET",
+
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5NzRiMTcwODk4YjQxM2E0OTNlYmJiIn0sImlhdCI6MTcwNDQxNzQ2NX0.JsrfmrmWKPSi7azA38SQTkUIgvBlVLlNh6OMI75MEYU",
+      },
+    });
+    const json = await response.json();
+    setNotes(json);
+  };
+
+  // Add a Note
+  const addNote = async (title, description, tag) => {
+    const response = await fetch(`${host}/api/notes/addnote`, {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5NzRiMTcwODk4YjQxM2E0OTNlYmJiIn0sImlhdCI6MTcwNDQxNzQ2NX0.JsrfmrmWKPSi7azA38SQTkUIgvBlVLlNh6OMI75MEYU",
+      },
+
+      body: JSON.stringify({ title, description, tag }),
+    });
+
+    const note = await response.json();
+
+    setNotes(notes.concat(note));
+  };
+
+  // Delete a Note
+  const deleteNote = async (id) => {
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5NzRiMTcwODk4YjQxM2E0OTNlYmJiIn0sImlhdCI6MTcwNDQxNzQ2NX0.JsrfmrmWKPSi7azA38SQTkUIgvBlVLlNh6OMI75MEYU",
+      },
+    });
+
+    const json = await response.json();
+    console.log(json);
+
+    const newNotes = notes.filter((note) => {
+      return note._id !== id;
+    });
+    setNotes(newNotes);
+  };
+
+  // Update a Note
+  const editNote = async (id, title, description, tag) => {
+    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+      method: "PUT",
+
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5NzRiMTcwODk4YjQxM2E0OTNlYmJiIn0sImlhdCI6MTcwNDQxNzQ2NX0.JsrfmrmWKPSi7azA38SQTkUIgvBlVLlNh6OMI75MEYU",
+      },
+
+      body: JSON.stringify({ title, description, tag }),
+    });
+    const json = await response.json();
+    console.log(json);
+
+    let newNotes = JSON.parse(JSON.stringify(notes));
+
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = notes[index];
+      if (element._id === id) {
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
+      }
+    }
+    setNotes(newNotes);
+  };
+  return (
+    <NoteContext.Provider
+      value={{ notes, deleteNote, addNote, editNote, getAllNotes }}
+    >
+      {props.children}
+    </NoteContext.Provider>
+  );
+};
 
 export default NoteState;
